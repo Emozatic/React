4903a17367b3b82f2237257ead4083dda3a50276
Lucky Sah <lk4179869@gmail.com>
2026-04-25 19:44:23 +0530

    initial commit

---
+++ basic-react-app/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Title from "./Title";
+
+// function Title(){
+//   return (
+//     <h1>This is title</h1>
+//   )
+// }
+
+function App() {
+  return (
+    <div>
+      <Title/>
+      <h1>This is first heading</h1>
+      <h2>This is second heading</h2>
+      <p>This is paragraph</p>
+    </div>
+  )
+}
+
+export default App
