033cf537a19968baa87dcabe2c9360ce038fc336
Lucky Sah <lk4179869@gmail.com>
2026-04-26 18:30:07 +0530

    adding array and object

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,10 +1,13 @@
 import "./Product.css";
-function Product({title,price}){
+function Product({title,price,features,features2}){
     return (
         <div className="mainBox">
         <h1>{title}</h1>
         <p>Price: {price}</p>
+        <p>{features}</p>
+        <p>{features2.a}</p>
         </div>
+
     )
 }
 
--- basic-react-app/src/ProductTab.jsx
+++ basic-react-app/src/ProductTab.jsx
@@ -1,11 +1,13 @@
 import Product from "./Product";
 
 function ProductTab(){
+    //const option= ["hi-tech","demanding"];
+    //const option2= {a:"technology", b:"value for money"};
     return(
         <>
-        <Product title="phone" price={30000}/>
-        <Product title="laptop" price={80000}/>
-        <Product title="Tab" price={40000}/>
+        <Product title="phone" price={30000} features={["hi-tech","demanding"]} features2={{a:"technology", b:"value for money"}}/>
+        <Product title="laptop" price={80000} features={["hi-tech","demanding"]} features2={{a:"technology", b:"value for money"}}/>
+        <Product title="Tab" price={40000} features={["hi-tech","demanding"]} features2={{a:"technology", b:"value for money"}}/>
         </>
     )
 }
