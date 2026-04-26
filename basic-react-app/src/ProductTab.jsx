fa2107496ef7692cfe5a6c6e028255427c9784ec
Lucky Sah <lk4179869@gmail.com>
2026-04-26 18:34:01 +0530

    rendering arrays elements

--- basic-react-app/src/ProductTab.jsx
+++ basic-react-app/src/ProductTab.jsx
@@ -1,13 +1,13 @@
 import Product from "./Product";
 
 function ProductTab(){
-    //const option= ["hi-tech","demanding"];
-    //const option2= {a:"technology", b:"value for money"};
+    const option= [<li>high-tech</li>, <li>durable</li>];
+    const option2= {a:"technology", b:"value for money"};
     return(
         <>
-        <Product title="phone" price={30000} features={["hi-tech","demanding"]} features2={{a:"technology", b:"value for money"}}/>
-        <Product title="laptop" price={80000} features={["hi-tech","demanding"]} features2={{a:"technology", b:"value for money"}}/>
-        <Product title="Tab" price={40000} features={["hi-tech","demanding"]} features2={{a:"technology", b:"value for money"}}/>
+        <Product title="phone" price={30000} features={option} features2={option2}/>
+        <Product title="laptop" price={80000} features={option} features2={option2}/>
+        <Product title="Tab" price={40000} features={option} features2={option2}/>
         </>
     )
 }
